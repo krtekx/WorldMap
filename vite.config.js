46f783d61350286dcbce75869fa9615cc1f2ef23
krtekx <radim.petruska@gmail.com>
2025-11-30 22:21:33 +0100

    Fix CSP eval() issue with Vite config v3.4.3

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    base: '/',
+    build: {
+        // Disable minification features that use eval()
+        minify: 'terser',
+        terserOptions: {
+            compress: {
+                // Disable eval-based optimizations
+                unsafe: false,
+                unsafe_comps: false,
+                unsafe_Function: false,
+                unsafe_math: false,
+                unsafe_methods: false,
+                unsafe_proto: false,
+                unsafe_regexp: false,
+                unsafe_undefined: false
+            }
+        },
+        // Ensure source maps don't use eval
+        sourcemap: false
+    }
+})
